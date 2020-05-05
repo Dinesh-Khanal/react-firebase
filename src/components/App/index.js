aa0701cdc70d8eb6f6476a25025993c79e86eb8b
Dinesh Khanal <dineshkhanal@gmail.com>
2020-05-05 19:16:34 +0545

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "../HomePage";
+import Dashboard from "../Dashboard";
+import Login from "../Login";
+import Register from "../Register";
+import firebase from "../firebase";
+
+const theme = createMuiTheme();
+
+export default function App() {
+  const [FirebaseInitialized, setFirebaseInitialized] = useState(false);
+
+  useEffect(() => {
+    firebase.isInitialized().then((val) => {
+      setFirebaseInitialized(val);
+    });
+  });
+
+  return FirebaseInitialized !== false ? (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/login" component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
+  ) : (
+    <div id="loader">CircularProgress</div>
+  );
+}
---
+++ src/components/firebase.js
@@ -0,0 +1,69 @@
+import app from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/firestore";
+import "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const config = {
+  apiKey: "AIzaSyAJN_fNubHpDGBtG0JT3W5I_84FmVrpnRc",
+  authDomain: "sample-e057a.firebaseapp.com",
+  databaseURL: "https://sample-e057a.firebaseio.com",
+  projectId: "sample-e057a",
+  storageBucket: "sample-e057a.appspot.com",
+  messagingSenderId: "1026206767527",
+  appId: "1:1026206767527:web:5696e21de7661aba037e82",
+};
+
+// Initialize Firebase
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+    this.auth = app.auth();
+    this.db = app.firestore();
+  }
+
+  login(email, password) {
+    return this.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  logout() {
+    return this.auth.signOut();
+  }
+
+  async register(name, email, password) {
+    await this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth.currentUser.updateProfile({
+      displayName: name,
+    });
+  }
+  addQuote(quote) {
+    if (!this.auth.currentUser) {
+      return alert("Not authorized");
+    }
+
+    return this.db
+      .doc(`users_codedamn_video/${this.auth.currentUser.uid}`)
+      .set({
+        quote,
+      });
+  }
+
+  isInitialized() {
+    return new Promise((resolve) => {
+      this.auth.onAuthStateChanged(resolve);
+    });
+  }
+
+  getCurrentUsername() {
+    return this.auth.currentUser && this.auth.currentUser.displayName;
+  }
+
+  async getCurrentUserQuote() {
+    const quote = await this.db
+      .doc(`users_codedamn_video/${this.auth.currentUser.uid}`)
+      .get();
+    return quote.get("quote");
+  }
+}
+export default new Firebase();
